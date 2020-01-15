946ebfc8e763d96186ab713563d5c781c73ddbb0
Dylan McDonald <dylan@deepdivedylan.io>
2020-01-15 12:29:59 -0800

    added transaction register

---
+++ src/main/java/io/deepdivedylan/hawthornloan/TransactionRegister.java
@@ -0,0 +1,16 @@
+package io.deepdivedylan.hawthornloan;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class TransactionRegister {
+    List<Transaction> transactions;
+
+    public TransactionRegister() {
+        transactions = new LinkedList<Transaction>();
+    }
+
+    public void importFromCSV(String filename) {
+
+    }
+}
