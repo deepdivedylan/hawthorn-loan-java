1ae6e185b519a273763fd0c24d861395be1116a0
Dylan McDonald <dylan@deepdivedylan.io>
2020-01-15 12:53:45 -0800

    added CSV import method

--- src/main/java/io/deepdivedylan/hawthornloan/TransactionRegister.java
+++ src/main/java/io/deepdivedylan/hawthornloan/TransactionRegister.java
@@ -1,5 +1,10 @@
 package io.deepdivedylan.hawthornloan;
 
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVRecord;
+
+import java.io.FileReader;
+import java.io.Reader;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -11,6 +16,28 @@ public class TransactionRegister {
     }
 
     public void importFromCSV(String filename) {
-
+        /**
+         * row map from Hawthorn Bank:
+         *
+         * row[0]: account name/number (always same value)
+         * row[1]: serial number (always 0000000000)
+         * row[2]: transaction comment
+         * row[3]: transaction date in m/d/y format
+         * row[4]: transaction amount
+         * row[5]: transaction multiplier (CR or DR)
+         **/
+        try {
+            Reader input = new FileReader(filename);
+            Iterable<CSVRecord> csvRecords = CSVFormat.DEFAULT.withFirstRecordAsHeader().parse(input);
+            for (CSVRecord csvRecord : csvRecords) {
+                String transactionComment = csvRecord.get(2);
+                String transactionDate = csvRecord.get(3);
+                double transactionAmount = Double.parseDouble(csvRecord.get(4));
+                String transactionType = csvRecord.get(5);
+                transactions.add(new Transaction(transactionAmount, transactionComment, transactionDate, transactionType));
+            }
+        } catch (Exception exception) {
+            System.err.println("Exception: " + exception.getMessage());
+        }
     }
 }
